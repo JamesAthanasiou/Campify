cd01131a6ae689a3b710889bb433e3babbdf4d9f
James Athanasiou <james.athanasiou@live.com>
2019-12-18 17:55:58 -0500

    improve profile layout

--- models/user.js
+++ models/user.js
@@ -4,8 +4,9 @@ var passportLocalMongoose = require("passport-local-mongoose");
 var UserSchema = new mongoose.Schema({
 	username: String,
     password: String,
-    email: String,
-    avatar: String
+    email: { type: String, default: ""},
+    avatar: { type: String, default: "https://images.unsplash.com/photo-1504697570352-47c3bbc09af9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=752&q=80"},
+    description: { type: String, default: "This user has nothing to say about themself yet!"}
 });
 
 UserSchema.plugin(passportLocalMongoose);
